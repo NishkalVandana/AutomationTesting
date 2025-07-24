18fd941bc3f4f56106c9d65dbd5ec27e181c4d30
Nishkal1249 <nishkalvandaana01@gmail.com>
2025-07-24 21:12:34 +0530

    Added geolocation automation using ChromeOptions in Selenium

---
+++ src/test/java/AdvancedWebDriverFeatures/LocationAccess.java
@@ -0,0 +1,36 @@
+package AdvancedWebDriverFeatures;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LocationAccess {
+
+    @Test
+    public void allowlocation()
+    {
+        ChromeOptions options= new ChromeOptions();
+        HashMap<String,Object> prefs=new HashMap<>();
+        prefs.put("profile.default_content_setting_value.geolocation",1);
+        options.setExperimentalOption("prefs",prefs);
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/geolocation.html");
+        driver.findElement(By.tagName("button")).click();
+        WebElement coords = driver.findElement(By.id("coordinates"));
+        wait.until(ExpectedConditions.visibilityOf(coords));
+        Assert.assertTrue(coords.isDisplayed(), "String message");
+
+    }
+}
