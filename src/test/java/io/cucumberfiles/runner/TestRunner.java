26724f0d0383556367eae3fa71b884891563ebde
Nishkal1249 <nishkalvandaana01@gmail.com>
2025-08-02 21:22:44 +0530

    Project to create cucumber report using jenkin

--- src/test/java/io/cucumberfiles/runner/TestRunner.java
+++ src/test/java/io/cucumberfiles/runner/TestRunner.java
@@ -2,9 +2,9 @@ package io.cucumberfiles.runner;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/cart.feature",   // ✅ your feature file path
         glue = "io.cucumberfiles.steps",// ✅ step definitions package
@@ -12,5 +12,5 @@ import org.junit.runner.RunWith;
         monochrome = true,
         dryRun = false
 )
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
