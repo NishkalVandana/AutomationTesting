965533b2cce2901cf86cc3044c7f4dfd192cdcb6
Nishkal1249 <nishkalvandaana01@gmail.com>
2025-08-02 21:18:40 +0530

    Project to generate cucumber report

--- src/test/java/io/cucumberfiles/runner/TestRunner.java
+++ src/test/java/io/cucumberfiles/runner/TestRunner.java
@@ -6,9 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/Resources/cart.feature",   // ✅ your feature file path
+        features = "src/test/resources/cart.feature",   // ✅ your feature file path
         glue = "io.cucumberfiles.steps",// ✅ step definitions package
-        tags = "@smoke",
         plugin = {"pretty","json:target/cucumber.json"},
         monochrome = true,
         dryRun = false
