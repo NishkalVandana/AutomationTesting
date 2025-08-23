bbbd440782b9e1acac5487c371429c793a3bdd48
Nishkal1249 <nishkalvandaana01@gmail.com>
2025-08-23 16:13:41 +0530

    Models used for RestAssured based CRUD operations

---
+++ src/test/java/models/petmodel.java
@@ -0,0 +1,53 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import models.Category;
+import java.util.Locale;
+
+@JsonIgnoreProperties({"photoUrls", "tags"})
+public class petmodel {
+
+    private int id;
+    private String name;
+    private Category category;
+    private String status;
+
+    public petmodel(int id, String name, Category category, String status) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
